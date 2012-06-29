c4bb26ccc25d38481b97371c96ea71df653f5a3f
cytec <iamcytec@googlemail.com>
2012-06-29 15:54:00 +0200

    changed log format

--- synoindex/logger.py
+++ synoindex/logger.py
@@ -3,7 +3,7 @@ from synoindex import config
 
 logger = logging.getLogger("SynoDLNAtrakt")
 hdlr = logging.FileHandler("SynoDLNAtrakt.log")
-formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+formatter = logging.Formatter('%(asctime)s %(levelname)-8s: %(message)s','%d.%m.%Y %H:%M:%S')
 hdlr.setFormatter(formatter)
 logger.addHandler(hdlr) 
 logger.setLevel(logging.DEBUG)
