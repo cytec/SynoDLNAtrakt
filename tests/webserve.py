645871410ccfde7fc24b1eed45a49d6069223703
cytec <iamcytec@googlemail.com>
2012-07-03 19:08:15 +0200

    first tests for webserve

---
+++ tests/webserve.py
@@ -0,0 +1,26 @@
+import cherrypy
+from Cheetah.Template import Template
+
+from synodlnatrakt import helper
+from synodlnatrakt.logger import logger
+from synodlnatrakt import config
+from synodlnatrakt import db
+
+class MainWebsite(object):
+	def index(self):
+		return 'Please check your <a href="settings">settings</a><br /> or your <a href="log">logfile</a>'
+	index.exposed = True
+    
+	def settings(self):
+		return "Hello world!"
+	settings.exposed = True
+	index.exposed = True
+
+	def log(self):
+		logcontent = open('SynoDLNAtrakt.log').read()
+		message = "<code>{0}</code>".format(logcontent)
+		return logcontent
+	log.exposed = True
+	index.exposed = True
+
+cherrypy.quickstart(MainWebsite())
