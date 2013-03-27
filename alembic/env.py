0399861d87ce1deae74bdc9e74d3be17f66021c9
cytec <iamcytec@googlemail.com>
2013-03-27 18:27:33 +0100

    fix for alembic autogeneration

--- alembic/env.py
+++ alembic/env.py
@@ -15,7 +15,15 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+
+import os
+import sys
+base_path = os.path.dirname(os.path.abspath(__file__))
+
+# Insert local directories into path
+sys.path.append(os.path.join(base_path, '..'))
+from synodlnatrakt.db import Base
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
